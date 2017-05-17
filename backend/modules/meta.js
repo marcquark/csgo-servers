50183e4347bc096e17d0d86924e3395f3e78c544
Marc <github@mleuser.de>
2017-05-17 22:42:57 +0200

    return query results to client

--- backend/modules/meta.js
+++ backend/modules/meta.js
@@ -4,21 +4,24 @@ var express = require('express');
 var router  = express.Router();
 
 router.get('/', function(req, res) {
+    var meta = {};
     hub.mariaSqlClientRO.query('SELECT `id`,`name` FROM `tags`', function(err, rows) {
         if(err) {
             res.sendStatus(500);
-            // TODO: Proper error handling
             return;
         }
-        // TODO: concatenate the results to a nice JSON reply
+
+        meta.tags = rows;
+
         hub.mariaSqlClientRO.query('SELECT `id`,`name` FROM `types`', function(err, rows) {
             if(err) {
                 res.sendStatus(500);
-                // TODO: Proper error handling
                 return;
             }
-            // TODO: concatenate the results to a nice JSON reply
-            res.sendStatus(200); // TODO: send out the reply here instead of a status code
+
+            meta.types = rows;
+
+            res.send(meta);
         });
     });
 });
